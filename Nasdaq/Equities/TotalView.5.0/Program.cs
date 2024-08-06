ff36f20ab27eb7276500d91118394572cf4b9ee7
Open-Markets-Initiative <contact.omi@protonmail.com>
2024-08-05 19:55:31 -0500

    Update example program

--- Nasdaq/Equities/TotalView.5.0/Program.cs
+++ Nasdaq/Equities/TotalView.5.0/Program.cs
@@ -6,7 +6,7 @@ namespace Nasdaq.TotalView
     class Program
     {
         public unsafe static void Main(string[] args) {
-            string pcap = @"C:\Users\wwteg\Documents\GitHub\Data\Nasdaq\Nasdaq.Equities.TotalView.Itch.v5.0\OrderDeleteMessage.D.pcap";
+            string pcap = @"";
             
             var device = new SharpPcap.LibPcap.CaptureFileReaderDevice(pcap);
             device.Open();
@@ -28,9 +28,9 @@ namespace Nasdaq.TotalView
 
             fixed (byte* buffer = &current.Data[0]) 
             {
-                var packetHeader = *(PacketHeader.Layout *)(buffer + 46);
-                var session = packetHeader.Session.Value;
-                var count = packetHeader.Sequence.Value;
+                var packet = *(PacketHeader *)(buffer + 46);
+                var session = packet.Session;
+                var count = packet.Sequence;
             }
         }
     }
